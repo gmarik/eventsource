894d9fa36d701676462128c6a3f7d9f777f46fab
gmarik <gmarik@gmail.com>
2016-03-10 01:31:13 -0500

    tests

--- eventsource_test.go
+++ eventsource_test.go
@@ -9,6 +9,8 @@ import (
 	"bytes"
 	"log"
 	"os"
+	"sync"
+	"time"
 )
 
 type TestResponseWriter struct {
@@ -31,29 +33,38 @@ func (m *TestResponseWriter) Close() {
 	m.gone <- true
 }
 
-func TestEventStream(t *testing.T) {
+func TestSingleClient(t *testing.T) {
 	if testing.Verbose() {
 		Vlog = log.New(os.Stdout, "", log.LstdFlags)
 	}
 
 	es := New()
-	go es.Listen()
 
 	req, err := http.NewRequest("GET", "/", bytes.NewBufferString(""))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	w := NewTestResponseWriter()
+	n := 3000
+	starting := &sync.WaitGroup{}
+	stopping := &sync.WaitGroup{}
+	starting.Add(n)
+	stopping.Add(n)
 
-	done, started := make(chan struct{}), make(chan struct{})
+	clients := make([]*TestResponseWriter, n, n)
 
 	// process client
-	go func() {
-		close(started)
-		es.ServeHTTP(w, req)
-		close(done)
-	}()
+	for i := 0; i < n; i += 1 {
+		clients[i] = NewTestResponseWriter()
+		go func(w *TestResponseWriter) {
+			starting.Done()
+			es.ServeHTTP(w, req)
+			stopping.Done()
+		}(clients[i])
+	}
+
+	Vlog.Println("Starting ES")
+	go es.Listen()
 
 	events := []Event{
 		{Data: "Hello", ID: "1", Event: "e1"},
@@ -61,18 +72,28 @@ func TestEventStream(t *testing.T) {
 		{Data: "!!", ID: "3", Event: "e3"},
 	}
 
-	// wait for client goroutine
-	<-started
+	// wait for clients to connnect
+	Vlog.Println("Clients connecting")
+	starting.Wait()
 
+	<-time.After(100 * time.Millisecond)
+
+	Vlog.Println("Sending out events")
 	for _, e := range events {
 		es.Push(e)
 	}
 
-	// disconnect client
-	w.Close()
-	<-done
+	<-time.After(100 * time.Millisecond)
 
-	got := w.Body.String()
+	// disconnect clients
+	Vlog.Println("Clients leaving")
+	for _, w := range clients {
+		w.Close()
+	}
+
+	Vlog.Println("Clients disconnecting")
+	stopping.Wait()
+
 	exp := `id: 1
 event: e1
 data: Hello
@@ -86,9 +107,51 @@ event: e3
 data: !!
 
 `
-	if got != exp {
-		t.Errorf("\nExp: %v\nGot: %v", exp, got)
+	for i, w := range clients {
+		got := w.Body.String()
+		if got != exp {
+			t.Errorf("\nClient %d\nExp: %v\nGot: %v", i, exp, got)
+		}
 	}
+}
+
+func BenchmarkIt(t *testing.B) {
+	es := New()
+
+	req, err := http.NewRequest("GET", "/", bytes.NewBufferString(""))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n := 20
+	starting := &sync.WaitGroup{}
+	starting.Add(n)
+
+	clients := make([]*TestResponseWriter, n, n)
+
+	// process client
+	for i := 0; i < n; i += 1 {
+		clients[i] = NewTestResponseWriter()
+		go func(w *TestResponseWriter) {
+			starting.Done()
+			es.ServeHTTP(w, req)
+		}(clients[i])
+	}
+
+	go es.Listen()
+
+	starting.Wait()
+	<-time.After(500 * time.Millisecond)
+
+	Vlog.Println("Sending out events")
+
+	t.StartTimer()
+	for i := 1; i < t.N; i += 1 {
+		es.Push(Event{Data: "Hello", ID: "1", Event: "e1"})
+	}
+	t.StopTimer()
+
+	<-time.After(500 * time.Millisecond)
 }
 
 func TestEventWriter(t *testing.T) {

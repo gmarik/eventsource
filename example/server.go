eb85acee3e0f7c47655a5413d01a6bdbe62f2160
gmarik <gmarik@gmail.com>
2016-04-11 16:27:30 -0400

    Improved example

--- example/server.go
+++ example/server.go
@@ -22,54 +22,26 @@ func main() {
 	go func() {
 		for i := 0; ; i += 1 {
 			evt := sse.Event{
-				Event: "time",
-				Data:  time.Now().Format(time.RFC3339),
+				Event: "count",
+				Data:  fmt.Sprintf("%d", i),
 			}
 
 			done, _ := es.Push(evt)
-
 			<-done
+
 			<-time.After(1 * time.Second)
 		}
 	}()
 
-	http.Handle("/clock", sse.Headers(es))
+	http.Handle("/sharedc", sse.Headers(es))
+	http.Handle("/parallelc", sse.Headers(http.HandlerFunc(counter)))
 
-	// sends initial event
-	http.Handle("/custom", sse.Headers(custom(es)))
-	http.Handle("/counter", sse.Headers(http.HandlerFunc(counter)))
-	http.Handle("/counters", http.HandlerFunc(counters))
 	http.HandleFunc("/", index)
 
-	log.Println("Listening on localhost:7070")
+	log.Println("Listening on http://localhost:7070")
+	log.Println("Listening on https://localhost:7071")
 	go http.ListenAndServe(":7070", nil)
 	log.Fatal(http.ListenAndServeTLS(":7071", "example/cert.pem", "example/key.pem", nil))
-}
-
-func custom(es sse.SSE) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		wfcn, ok := w.(sse.WriteFlushCloseNotifier)
-		if !ok {
-			log.Println(http.ErrNotSupported)
-			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
-			return
-		}
-		wfcn.Flush()
-
-		err := sse.WriteEvent(wfcn, sse.Event{Event: "test", Data: "Hello"})
-		if err != nil {
-			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
-			return
-		}
-
-		conn := sse.NewConn(wfcn)
-		es.Join(conn)
-		defer es.Leave(conn)
-
-		if err := conn.Serve(es); err != nil {
-			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
-		}
-	})
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
@@ -97,18 +69,14 @@ func counter(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("example/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-	}
-
-	t.Execute(w, struct{}{})
+	render(w, "example/index.html", struct{}{})
 }
-func counters(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("example/counters.html")
+
+func render(w http.ResponseWriter, path string, kvs interface{}) {
+	t, err := template.ParseFiles(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 	}
 
-	t.Execute(w, struct{}{})
+	t.Execute(w, kvs)
 }
